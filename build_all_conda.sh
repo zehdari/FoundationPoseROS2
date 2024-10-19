3f57a3b4e72c7edf8b7c0b5f6b6da2ad4ab0521c
Ammar N. Abbas <ammar.n.abbas@gmail.com>
2024-10-19 17:05:40 +0100

    Add files via upload

---
+++ build_all_conda.sh
@@ -0,0 +1,29 @@
+PROJ_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Install pybind11
+cd ${PROJ_ROOT}/FoundationPose && git clone https://github.com/pybind/pybind11 && \
+    cd pybind11 && git checkout v2.10.0 && \
+    mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Release -DPYBIND11_INSTALL=ON -DPYBIND11_TEST=OFF && \
+    make -j6 && make install
+
+# Install Eigen
+cd ${PROJ_ROOT}/FoundationPose && wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz && \
+    tar xvzf ./eigen-3.4.0.tar.gz && \
+    cd eigen-3.4.0 && \
+    mkdir build && \
+    cd build && \
+    cmake .. && \
+    make install
+
+# Install mycpp
+cd ${PROJ_ROOT}/FoundationPose/mycpp/ && \
+rm -rf build && mkdir -p build && cd build && \
+cmake .. && \
+make -j$(nproc)
+
+# Install mycuda
+cd ${PROJ_ROOT}/FoundationPose/bundlesdf/mycuda && \
+rm -rf build *egg* *.so && \
+python3 -m pip install -e .
+
+cd ${PROJ_ROOT}
